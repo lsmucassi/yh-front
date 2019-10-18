2eaa6f7cd010f8d44bfbf47c461e8026c36195fb
lsmucassi <lindasmucassi@gmail.com>
2019-10-19 01:03:02 +0200

    contact us functionality 01

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,25 @@ console.log("it works");
 $(document).ready(function () {
     $('.contsubmit').click(function (event) {
         event.preventDefault();
+
+        var name, email, phone_number, subject, message, status;
+
+        if (email.length > 5 && email.include('@') && email.include('.')) {
+            //status
+        } else {
+            //status
+        }
+
+        if (subject.length >= 2) {
+            //status
+        } else {
+            //status
+        }
+
+        if (message.length >= 20) {
+            //status
+        } else {
+            //status
+        }
     })
 })
